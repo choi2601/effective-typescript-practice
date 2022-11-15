f6de246dd6815e031011e565d4db49e3297dad30
choi2601 <byeonghyeonchoe55@gmail.com>
2022-11-15 22:12:23 +0900

    item02

---
+++ ch01/item02/item02.ts
@@ -0,0 +1,13 @@
+// 예제 1
+function add(a, b) {
+  return a + b;
+}
+
+// 예제 2
+const el = document.getElementById("status");
+el.textContent = "Ready";
+
+if (el) {
+  el.textContent = "Ready";
+}
+el!.textContent = "Ready";
