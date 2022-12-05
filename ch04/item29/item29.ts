7006e21a0ffe61c78fea1d7efc3cdfb7a6eb1cb6
choi2601 <byeonghyeonchoe55@gmail.com>
2022-12-05 21:10:09 +0900

    item29: 사용할 때는 너그럽게, 생성할 때는 엄격하게 내용 수정

--- ch04/item29/item29.ts
+++ ch04/item29/item29.ts
@@ -15,9 +15,31 @@ type LngLat =
   | [number, number];
 
 declare function setCamera(camera: CameraOptions): void;
-declare function viewporForBounds(bounds: LngLatBounds): CacheQueryOptions;
+// 매개변수 타입의 범위가 넓으면 사용하기 편리하지만, 반환 타입의 범위가 넓으면 사용하기 불편
+declare function viewporForBounds(bounds: LngLatBounds): CameraOptions;
 
 type LngLatBounds =
   | { northeast: LngLat; southwest: LngLat }
   | [LngLat, LngLat]
   | [number, number, number, number];
+
+// 예제 2
+
+interface LngLatCorrect {
+  lng: number;
+  lat: number;
+}
+type LngLatLike =
+  | LngLatCorrect
+  | { lon: number; lat: number }
+  | [number, number];
+
+interface CameraCorrect {
+  center: LngLat;
+  zoom: number;
+  bearing: number;
+  pitch: number;
+}
+interface CameraOptions2 extends Omit<CameraCorrect, "center"> {
+  center?: LngLatLike;
+}
