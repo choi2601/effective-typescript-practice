f48288afb4f4e795b8e4adf658d887bb5d1845e2
choi2601 <byeonghyeonchoe55@gmail.com>
2022-12-10 16:51:53 +0900

    item36: 해당 분야의 용어로 타입 이름 짓기

--- ch04/item36/item36.ts
+++ ch04/item36/item36.ts
@@ -1,11 +1,20 @@
-# Ch04
+// 예제 1
+interface Animal {
+  name: string;
+  endangered: boolean;
+  habitat: string;
+}
 
-## Item35
+const leopard: Animal = {
+  name: "Snow Leopard",
+  endangered: false,
+  habitat: "tundra",
+};
 
-데이터가 아닌, API와 명세를 보고 타입 만들기
-
-### 정리
-
-명세를 기반으로 타입을 작성한다면 현재까지 경험한 데이터뿐만 아니라 사용 가능한 모든 값에 대해서 작동한다는 확신을 가질 수 있음
-
-코드의 구석 구석까지 타입 안전성을 얻기 위해 API 또는 데이터 형식에 대한 타입 생성을 고려해야 함
+interface Animal {
+  commonName: string;
+  genus: string;
+  species: string;
+  status: ConservationStatus;
+  climates: KoppenClimate[];
+}
